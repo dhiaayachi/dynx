d0f172612e8d3018939fdb14993d64b87eb537b6
dhayachi <dhia.ayachi@bell.ca>
2017-07-20 18:10:54 -0400

    Close connection and correct a syntax error

--- test/test_integration.py
+++ test/test_integration.py
@@ -9,16 +9,20 @@ class TestStringMethods(unittest.TestCase):
         connRouter.request("GET", "/google")
         response = connRouter.getresponse()
         self.assertEqual(response.status, 404)
+        connRouter.close()
+        connConfig.close()
 
     def test_200NoConfig(self):
         connRouter = http.client.HTTPConnection("localhost", 8666)
         connConfig = http.client.HTTPConnection("localhost", 8888)
         connConfig.request("GET","/configure?location=/google&upstream=http://www.google.com&ttl=10")
-        response = conn.getresponse()
+        response = connConfig.getresponse()
         self.assertEqual(response.status, 200)
         connRouter.request("GET", "/google")
-        response = conn.getresponse()
+        response = connRouter.getresponse()
         self.assertEqual(response.status, 200)
+        connRouter.close()
+        connConfig.close()
         
 
 
