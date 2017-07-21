30f0125622226423bafb6d47efac85e8a446c28b
Dhia Ayachi <dhia.ayachi@gmail.com>
2017-07-20 23:01:57 -0400

    Fix issue that make configure api return 404
    The ok and err variable are out of scope when we check for the error, fixed now

--- upstream_config.lua
+++ upstream_config.lua
@@ -2,11 +2,13 @@ local router = require "resty.router"
 local r = router:new("resty.router.redis_dns")
 
 local method = ngx.req.get_method()
+local ok = nil
+local err = nil
 if method == ngx.HTTP_GET then
-  local ok, err = r:set_route(ngx.var.arg_location,ngx.var.arg_upstream,ngx.var.arg_ttl)
+  ok, err = r:set_route(ngx.var.arg_location,ngx.var.arg_upstream,ngx.var.arg_ttl)
 end
 if method == ngx.HTTP_DELETE then
-  local ok, err = r:unset_route(ngx.var.arg_location)
+  ok, err = r:unset_route(ngx.var.arg_location)
 end
 
 if not ok then
